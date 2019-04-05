337a281021cb5d70afb09af1f3e1ef08fc83417c
Rene Dohmen (imac) <acidjunk@gmail.com>
2019-04-06 01:33:25 +0200

    Re #1

--- tests/unit_tests/developers/test_views.py
+++ tests/unit_tests/developers/test_views.py
@@ -14,6 +14,11 @@ def test_developer_detail_view_by_slug(user_client, developer):
     assert response.status_code == 200
 
 
+def test_developer_detail_view_by_slug_404(user_client, developer):
+    response = user_client.get(reverse('developers:developer-detail', args=["chuck-norris-ninja-coder"]))
+    assert response.status_code == 404
+
+
 def test_developer_list_view_no_permission(admin_client, developer):
     response = admin_client.get(reverse('developers:developer-list'))
     assert response.status_code == 302
